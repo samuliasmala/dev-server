62122c0f624f5f98f690cffd13ff9d159fd9eee2
Samuli Asmala <samuli.asmala@gmail.com>
2020-01-15 09:21:07 +0200

    Add workaround to fix incorrect WSL umask

--- bootstrap-wsl.sh
+++ bootstrap-wsl.sh
@@ -3,6 +3,16 @@
 
 # Restore .ssh folder (id_rsa, id_rsa.pub and config files)
 
+# https://github.com/microsoft/WSL/issues/352
+# Note: Bash on Windows does not currently apply umask properly
+# Should be fixed in WSL2
+if [ "$(umask)" = "0000" ]; then
+    echo >> $HOME/.bashrc
+    echo "# Note: Bash on Windows does not currently apply umask properly" >> $HOME/.bashrc
+    echo 'if [ "$(umask)" = "0000" ]; then umask 002; fi' >> $HOME/.bashrc
+    umask 002
+fi
+
 bash bootstrap-common.sh
 
 # https://stackoverflow.com/questions/45437824/postgresql-warning-could-not-flush-dirty-data-function-not-implemented
@@ -11,3 +21,5 @@ bash bootstrap-common.sh
 echo "fsync = off
 data_sync_retry = true" | sudo tee -a $(find /etc/postgresql -name postgresql.conf)
 sudo service postgresql restart
+
+echo "Start new shell session to apply umask correctly "
