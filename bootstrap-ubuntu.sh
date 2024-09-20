54a8aa05084730da186bc12896332d53e0060288
Samuli Asmala <samuli.asmala@gmail.com>
2024-09-20 09:43:41 +0300

    Install Docker completions

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -9,3 +9,6 @@ sudo systemctl restart postgresql
 # Install Docker
 curl https://get.docker.com | bash
 sudo usermod -aG docker $USER
+# Install Docker completions (bash-completion package is required)
+mkdir -p ~/.local/share/bash-completion/completions
+docker completion bash > ~/.local/share/bash-completion/completions/docker
